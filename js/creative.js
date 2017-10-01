17566b64fdd19de3fc4959c2d933311b422dc870
Amber Victoria <chrysalisvictoria@gmail.com>
2017-10-01 15:06:27 -0400

    added image grid

--- js/creative.js
+++ js/creative.js
@@ -28,6 +28,19 @@
         }
     })
 
+        // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
